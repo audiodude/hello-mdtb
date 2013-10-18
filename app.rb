6663c43f7157580b2ff10a139c44dafa3414dc72
Matthew Dillabough <mattdillabough@gmail.com>
2013-10-18 02:41:11 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+# Change this method so that the server path is /hello and the
+# name of the ERB file being rendered is also called hello.
+get '/hello' do
+  @names = ['Matt', 'Travis']
+  erb :hello
+end
+
+# When you request http://mdtb-37357.usw1.actionbox.io:3000
+# it really requests http://mdtb-37357.usw1.actionbox.io:3000/
+# The extra '/' is important. You're requesting the path '/' on
+# the server. This is known also as the 'root' path.
+
