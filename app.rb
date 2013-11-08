9ed27b28ee2caf27f2e794636e201d62a60e9fd3
Matthew Dillabough <mattdillabough@gmail.com>
2013-11-08 03:55:29 +0000

    Finishing parameter homework

--- app.rb
+++ app.rb
@@ -3,15 +3,23 @@ require 'sinatra'
 # Change this method so that the server path is /hello and the
 # name of the ERB file being rendered is also called hello.
 get '/hello' do
-    if params[:fname] or params[:city] or params[:state]
-      @city = params[:fname]
-      @fname = params[:city]
-      @state = params[:state]
-    else
-       @failure = "Oops that's an error"
-    end
+  if params[:fname] && params[:city] && params[:lname]
+    @city = params[:city]
+    @fname = params[:fname]
+    @lname = params[:lname]
+  else
+    @failure = "Oops that's an error"
+  end    
+  
+  if params[:state]
+    @state = params[:state]
+  else
+    @state = "CA"
+  end
+    
   erb :hello
 end
+
 
 # When you request http://mdtb-37357.usw1.actionbox.io:3000
 # it really requests http://mdtb-37357.usw1.actionbox.io:3000/
